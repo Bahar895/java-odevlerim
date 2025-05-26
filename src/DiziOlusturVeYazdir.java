682661c35b9b954b11b1417de522a2da17c22cf0
Bahar Aydın <bhrs_01@hotmail.com>
2025-05-26 15:51:35 +0300

    5 elemanlı dizisi ve for döngüsü ile yazdırma tamamlandı

---
+++ src/DiziOlusturVeYazdir.java
@@ -0,0 +1,10 @@
+public class DiziOlusturVeYazdir {
+    public static void main(String[] args) {
+        // 5 elemanlı int dizisi tanımlıyoruz
+        int[] sayilar = {10,20,30,40,50};
+        // For döngüsü ile dizinin tüm elemanlarını yazdırıyoruz
+        for (int i =0; i < sayilar.length; i++) {
+            System.out.println("Dizinin" + i + ".elemanı:"+ sayilar[i]);
+}
+}
+}
