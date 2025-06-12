2beedf736c698b3896e2b88604174df74fc0da00
Bahar Aydın <bhrs_01@hotmail.com>
2025-06-13 00:10:16 +0300

    HashSet ödevi eklendi

---
+++ src/crc/collections/MeyveSeti.java
@@ -0,0 +1,12 @@
+package crc.collections;
+import java.util.HashSet;
+public class MeyveSeti {
+    public static HashSet<String> meyveleriGetir() {
+        HashSet<String> meyveler = new HashSet<>();
+        meyveler.add("Elma");
+        meyveler.add("Elma");
+        meyveler.add("Muz");
+        meyveler.add("Kiraz");
+        return meyveler;
+    }
+}
--- src/crc/main/Main.java
+++ src/crc/main/Main.java
@@ -5,13 +5,20 @@ package crc.main;
 //    public static void main(String[] args) {
 //        Gun bugun = Gun.CUMA;
 //        System.out.println("Bugün günlerden: " + bugun);
-import crc.collections.SehirListesi;
-import java.util.ArrayList;
+//import crc.collections.SehirListesi;
+//import java.util.ArrayList;
+//public class Main {
+//    public static void main(String[] args) {
+//        ArrayList<String> sehirler = SehirListesi.sehirleriGetir();
+//        for (String sehir : sehirler) {
+//            System.out.println("Şehir: " + sehir);
+import crc.collections.MeyveSeti;
+import java.util.HashSet;
 public class Main {
     public static void main(String[] args) {
-        ArrayList<String> sehirler = SehirListesi.sehirleriGetir();
-        for (String sehir : sehirler) {
-            System.out.println("Şehir: " + sehir);
+        HashSet<String > meyveler = MeyveSeti.meyveleriGetir();
+        for (String meyve : meyveler) {
+            System.out.println("Meyve: " + meyve);
         }
 
     }
