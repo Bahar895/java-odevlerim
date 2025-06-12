bb25bee4a4103a9d02a267ddd3e6e9e3b70f8f3a
Bahar Aydın <bhrs_01@hotmail.com>
2025-06-12 23:47:55 +0300

    Arraylist ödevi eklendi

---
+++ src/crc/collections/SehirListesi.java
@@ -0,0 +1,12 @@
+package crc.collections;
+import java.util.ArrayList;
+
+public class SehirListesi {
+    public static ArrayList<String> sehirleriGetir() {
+        ArrayList<String> sehirler = new ArrayList<>();
+        sehirler.add("İzmir");
+        sehirler.add("Ankara");
+        sehirler.add("İstanbul");
+        return sehirler;
+    }
+}
--- src/crc/main/Main.java
+++ src/crc/main/Main.java
@@ -1,9 +1,18 @@
 package crc.main;
-import crc.model.Gun;
-
+//import crc.model.Gun;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        Gun bugun = Gun.CUMA;
+//        System.out.println("Bugün günlerden: " + bugun);
+import crc.collections.SehirListesi;
+import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
-        Gun bugun = Gun.CUMA;
-        System.out.println("Bugün günlerden: " + bugun);
+        ArrayList<String> sehirler = SehirListesi.sehirleriGetir();
+        for (String sehir : sehirler) {
+            System.out.println("Şehir: " + sehir);
+        }
+
     }
 }
